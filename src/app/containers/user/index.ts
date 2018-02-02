d5d957375d0134add105fdcb315b6188e4c6757c
Vasuki Vardhan <vasukivardhan96@gmail.com>
2018-02-02 18:23:10 +0530

    Initial Commit

---
+++ src/app/containers/user/index.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared/index';
+import { AppNavbarModule } from '../app-navbar';
+
+import { UserComponent } from './user.component';
+import { PlaylistsComponent } from './playlists';
+// import { PlaylistViewComponent, PlaylistResolver, PlaylistVideosResolver } from '@shared/components/playlist-view';
+
+import { AuthGuard } from './user.guard';
+import { UserPlayerService } from './user-player.service';
+import { routing } from './user.routing';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    AppNavbarModule,
+    routing
+  ],
+  declarations: [
+    UserComponent,
+    PlaylistsComponent
+  ],
+  exports: [
+    UserComponent
+  ],
+  providers: [
+    AuthGuard,
+    UserPlayerService,
+    // PlaylistResolver,
+    // PlaylistVideosResolver
+  ]
+})
+export class UserModule { }
